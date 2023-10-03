2b70c203de8170feeb0ebfb5d6a1e6a23e5cb91d
Alex <alexpvn69@gmail.com>
2023-10-03 22:49:13 +0300

    ts

--- client/services/getAllArticles.ts
+++ client/services/getAllArticles.ts
@@ -2,9 +2,9 @@
 
 import { IArticle } from '@/types/common';
 
-export const getAllArticles = async (): Promise<IArticle[] | undefined> => {
+export const getAllArticles = async () => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/articles`, {
+    const response = await fetch(`http://localhost:4000/api/articles`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
